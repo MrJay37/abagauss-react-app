4f73b1e1c9071deb1324218cae4e833a083acedd
Chisako Goza <gozachisako@gmail.com>
2024-08-27 19:09:06 -0400

    add validation function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import TopPage from './TopPage';
+import "./App.css";
+import TopPage from "./TopPage";
 import "./styles/Global.scss";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme();
+
 function App() {
   return (
-    <div className="App">
-      <TopPage/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <TopPage />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/TopPage.js
+++ src/TopPage.js
@@ -1,47 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TopPage.scss";
 import TextField from "@mui/material/TextField";
-// import { ThemeProvider, createTheme } from "@mui/material/styles";
-import Box from '@mui/material/Box';
-
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
 const TopPage = () => {
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [passMessage, setPassMessage] = useState("");
+
+  const subForm = (event) => {
+    event.preventDefault();
+
+    const usernamePattern =
+      /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+
+    const passwordPattern = /^[a-zA-Z0-9!@#$%^&*]{6,}$/;
+    const account = event.target.elements.accId.value;
+    const password = event.target.elements.password.value;
+    let isRight = true;
+
+    //Validation for Username
+    if (usernamePattern.test(account)) {
+      console.log("Valid Username:", account);
+      setUsernameMessage("");
+    } else {
+      console.log("Invalid Username:", account);
+      setUsernameMessage("※Please enter the proper Username");
+      isRight = false;
+    }
+
+    if (passwordPattern.test(password)) {
+      console.log("Valid password:", password);
+      setPassMessage("");
+    } else {
+      console.log("Invalid password:", password);
+      setPassMessage("※Please enter the proper password");
+      isRight = false;
+    }
+
+    if (isRight) {
+      console.log("Form is valid. Ready to submit!");
+    }
+  };
+
   return (
-
-      <div className="topPageWrap">
-        <h1 className="topTitle">Abaguss</h1>
-
- 
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
-
-        {/* <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
-    </Box> */}
-    
-        {/* <div className="loginWrap">
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-          <TextField id="filled-basic" label="Filled" variant="filled" />
-          <TextField id="standard-basic" label="Standard" variant="standard" />
-        </div> */}
-
-      </div>
-
+    <div className="topPageWrap">
+      <h1 className="topTitle">Abaguss</h1>
+      <form
+        className="loginWrap"
+        // action="success.html"
+        method="get"
+        onSubmit={subForm}
+      >
+        <TextField id="accId" label="Username" variant="outlined" />
+        <div className="message">{usernameMessage}</div> <br />
+        <TextField id="password" label="Password" variant="outlined" />
+        <div className="password">{passMessage}</div> <br />
+        <Button id="submit" variant="outlined" type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 };
 
 export default TopPage;
+
+// //Validation for Password
+// if (password.length <= 5) {
+//   document.getElementById("txtId2").innerHTML =
+//     "※半角英数字5文字以上で入力してください";
+//   isRight = false;
+// } else {
+//   document.getElementById("txtId2").innerHTML = "";
+// }
